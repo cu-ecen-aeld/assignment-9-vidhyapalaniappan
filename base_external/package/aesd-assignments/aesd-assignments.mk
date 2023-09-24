9a5e882dd09cc5590c611b68aa2632a32bc0b175
Vidhya Palaniappan <vipa4058@aesd-students.colorado.edu>
2023-09-24 15:55:42 +0000

    added the full commit id

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 4763292
+AESD_ASSIGNMENTS_VERSION = 80d5e4f1773f5b417026c9882a2c93b55fd3aec5
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
