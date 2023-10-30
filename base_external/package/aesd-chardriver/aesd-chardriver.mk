fbdf1ca7c2b90e8c47c6ec22cc04634fa165e900
vidhyapalaniappan <vpa4058@colorado.edu>
2023-10-29 23:44:38 -0600

    A8 - complete

--- base_external/package/aesd-chardriver/aesd-chardriver.mk
+++ base_external/package/aesd-chardriver/aesd-chardriver.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_CHARDRIVER_VERSION = 101e8e5cc8c36f7bee230a38424619ab26aeb521
+AESD_CHARDRIVER_VERSION = 17ae358933f9a6267b0aaec211aa40eddda4b570
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
